22486592d977e9672bab1337c6c26d7fef9855de
malaya-01 <practice.git.963@gmail.com>
2025-04-10 12:23:02 +0530

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center text-white text-6xl h-screen"><h1>Welcome to Expense Tracker.</h1></div>
+  );
+}
